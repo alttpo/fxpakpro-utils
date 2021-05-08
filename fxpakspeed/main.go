6c26a37d92f33e5a4802223eeb8187cb405664b8
jsd1982 <james.jdunne@gmail.com>
2021-05-08 08:16:07 -0500

    added test

--- fxpakspeed/main.go
+++ fxpakspeed/main.go
@@ -222,6 +222,70 @@ writeloop:
 	runtime.UnlockOSThread()
 }
 
+func writeTestSpinLoopMeasure(f serial.Port) {
+	runtime.LockOSThread()
+
+	// SNES master clock ~= 1.89e9/88 Hz
+	// SNES runs 1 scanline every 1364 master cycles
+	// Frames are 262 scanlines in non-interlace mode (1 scanline takes 1360 clocks every other frame)
+	// 1 frame takes 357366 master clocks
+	const snes_frame_clocks = (261 * 1364) + 1362
+
+	const snes_frame_nanoclocks = snes_frame_clocks * 1_000_000_000
+
+	// 1 frame takes 0.016639356613757 seconds
+	// 0.016639356613757 seconds = 16,639,356.613757 nanoseconds
+
+	const snes_frame_time_nanoseconds_int = int(snes_frame_nanoclocks) / (int(1.89e9) / int(88))
+	const snes_frame_time_nanoseconds_flt = snes_frame_nanoclocks / ((1.89e9) / 88)
+
+	// Perform some timing tests:
+	const expectedBytes = 0xFF
+	const expectedPaddedBytes = 0x100
+	sb := makeVGET(0xF50000, expectedBytes)
+
+	log.Printf("VGET command:\n%s\n", hex.Dump(sb))
+
+	// duration between VGETs:
+	const dur = time.Nanosecond * time.Duration(snes_frame_time_nanoseconds_int)
+
+	last_t := time.Now()
+
+writeloop:
+	for i := 0; i < 600; i++ {
+		for time.Since(last_t) < dur {
+		}
+		t := time.Now()
+
+		//log.Printf("delta %v ns; %d frames remaining\n", t.Sub(last_t).Nanoseconds(), 600 - frames)
+		//fmt.Printf("%v\n", t.Sub(last_t).Nanoseconds())
+		last_t = t
+
+		// write:
+		log.Printf("write(VGET)\n")
+		n, err := f.Write(sb)
+		if err != nil {
+			log.Printf("write(): %v\n", err)
+			continue
+		}
+		log.Printf("write(): wrote %d bytes\n", n)
+		if n != len(sb) {
+			log.Printf("write(): expected to write 64 bytes but wrote %d\n", n)
+			continue
+		}
+
+		// read:
+		data := readData(f, expectedPaddedBytes, expectedBytes)
+		if data == nil {
+			continue writeloop
+		}
+		//log.Printf("VGET response:\n%s\n", hex.Dump(data))
+		log.Printf("[$10] = $%02x; [$1A] = $%02x\n", data[0x10], data[0x1A])
+	}
+
+	runtime.UnlockOSThread()
+}
+
 func makeVGET(addr uint32, size uint8) []byte {
 	sb := make([]byte, 64)
 	sb[0] = byte('U')
